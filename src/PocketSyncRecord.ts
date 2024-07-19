6381d8b4d145966e87313580e72505bd177e1199
Alex Navarro <alexkmf96@gmail.com>
2024-07-19 11:51:48 -0700

    feat: pocketsync updates
    Updated PocketSyncRecord to include `update` method. Updated `PocketSyncRecordList` to include `updateRecord` and `deleteRecord` methods. Other cleanup.

--- src/PocketSyncRecord.ts
+++ src/PocketSyncRecord.ts
@@ -3,28 +3,39 @@ import type { RecordModel } from "pocketbase";
 
 export class PocketSyncRecord<T extends RecordModel> {
     #pb: Pocketbase;
-    #collectionName: string;
-    public loading = true;
+    public collectionName: string;
     public id: string;
-    public data: T;
+    public record: T;
 
     constructor(record: T, pb: Pocketbase, collection: string) {
         this.id = record.id;
-        this.#collectionName = collection;
+        this.collectionName = collection;
         this.#pb = pb;
-        this.data = record;
+        this.record = record;
     }
 
     public async delete() {
-        if (!this.data) {
+        if (!this.record) {
             throw new Error("Record ID required");
         }
 
         try {
-            this.#pb.collection(this.#collectionName).delete(this.data.id);
+            this.#pb.collection(this.collectionName).delete(this.record.id);
         } catch (error) {
             console.error(error);
             throw new Error("Error deleting record");
+        }
+    }
+
+    public async update(updatedRecord: Partial<T>) {
+        if (!updatedRecord) {
+            throw new Error("updatedRecord required!");
+        }
+
+        try {
+            await this.#pb.collection(this.collectionName).update<T>(this.record.id, updatedRecord);
+        } catch (error) {
+            console.error("Error updating item: ", error);
         }
     }
 }
